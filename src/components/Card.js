e1de61b2ae6319f88abe52260c37e73b60b63af6
Roiney Beal <nei_beal@hotmail.com>
2022-05-24 17:34:11 -0300

    test

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Card() {
+    return ( 
+        <div>
+            <h2>{ nameProduct }</h2>
+            <img></img>
+        </div>
+     );
+}
+
+export default Card;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,38 @@
 import React from 'react';
 
-function Search() {
-  return (
-    <di>
-      <input type="text" />
-      <span data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </span>
-    </di>
-  );
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleClick = () => {
+    const { name, value } = this.state;
+    console.log(name);
+    console.log(value)
+  }
+
+  render() {
+    const { inputValue } = this.state;
+    return (
+      <div>
+        <input type="text" data-testid="query-input" name="inputValue" value={ inputValue } onChange={ this.handleChange } />
+        <button data-testid="query-button" onClick={ this.handleClick }>Pesquisar</button>
+        <p>
+          <span data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </span>
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Search;
