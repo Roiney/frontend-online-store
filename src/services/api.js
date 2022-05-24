64634bd4f908baaeada789c3919d20969a0e919b
Kevin Li <Kevinstart14@gmail.com>
2022-05-24 15:59:37 -0300

    lint

--- src/services/api.js
+++ src/services/api.js
@@ -1,51 +1,55 @@
 export async function getCategories() {
   // Implemente aqui
-const request = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
-const requestJson = await request.json();
-return requestJson;
+  const request = await
+  fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const requestJson = await request.json();
+  return requestJson;
 }
 
 const categoria = async (palavra) => {
-  const categoria = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${palavra}`)
-  const categoriaJson = await categoria.json();
-  return categoriaJson
-}
+  const categorias = await
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${palavra}`);
+  const categoriaJson = await categorias.json();
+  return categoriaJson;
+};
 
 const termo = async (b) => {
-  const termo = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${b}`)
-  const termoJson = await termo.json();
-  return termoJson
-}
+  const termos = await
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${b}`);
+  const termoJson = await termos.json();
+  return termoJson;
+};
 
-const categoryTermo = async (a,b) => {
-  const api = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${a}&q=${b}`)
+const categoryTermo = async (a, b) => {
+  const api = await
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${a}&q=${b}`);
   const fetchTudo = await api.json();
-  return fetchTudo
-}
+  return fetchTudo;
+};
 
 const detalhes = async (a) => {
-  const api = await fetch(`https://api.mercadolibre.com/items/${a}`)
+  const api = await
+  fetch(`https://api.mercadolibre.com/items/${a}`);
   const json = await api.json();
-  return json
-}
+  return json;
+};
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
- if (query === undefined) {
-   const category = await categoria(categoryId);
-   return category
- }
- if (categoryId === undefined) {
-   const word = await termo(query);
-   return word
- }
- if(query === undefined || categoryId === undefined){
-   const info = await detalhes(query);
-   return info
- }
- if(query !== undefined || categoryId !== undefined){
-   const tudo = await categoryTermo(categoryId, query);
-   return tudo
- }
+  if (query === undefined) {
+    const category = await categoria(categoryId);
+    return category;
+  }
+  if (categoryId === undefined) {
+    const word = await termo(query);
+    return word;
+  }
+  if (query === undefined || categoryId === undefined) {
+    const info = await detalhes(query);
+    return info;
+  }
+  if (query !== undefined || categoryId !== undefined) {
+    const tudo = await categoryTermo(categoryId, query);
+    return tudo;
+  }
 }
-
