8cce1562bc939eae064a1d0c1142912bb04acb6b
Roiney Beal <nei_beal@hotmail.com>
2022-05-24 15:25:53 -0300

    requesito 02

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Search() {
+  return (
+    <di>
+      <input type="text" />
+      <span data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</span>
+    </di>
+  );
+}
+
+export default Search;
