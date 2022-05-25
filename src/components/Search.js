1de3105798866495c0cfc8d72722423898fe069d
Roiney Beal <nei_beal@hotmail.com>
2022-05-25 09:06:55 -0300

    acho que foi

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Categories from './Categories';
 
-<<<<<<< HEAD
 class Search extends React.Component {
   constructor(props) {
     super(props);
@@ -19,36 +18,35 @@ class Search extends React.Component {
   handleClick = () => {
     const { name, value } = this.state;
     console.log(name);
-    console.log(value)
-  }
+    console.log(value);
+  };
 
   render() {
     const { inputValue } = this.state;
     return (
       <div>
-        <input type="text" data-testid="query-input" name="inputValue" value={ inputValue } onChange={ this.handleChange } />
-        <button data-testid="query-button" onClick={ this.handleClick }>Pesquisar</button>
+        <Link to="/carrinho" data-testid="shopping-cart-button">
+          Carrinho de compras
+        </Link>
+        <input
+          type="text"
+          data-testid="query-input"
+          name="inputValue"
+          value={ inputValue }
+          onChange={ this.handleChange }
+        />
+        <button data-testid="query-button" type="submit" onClick={ this.handleClick }>
+          Pesquisar
+        </button>
         <p>
           <span data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </span>
         </p>
+        <Categories />
       </div>
     );
   }
-=======
-function Search() {
-  return (
-    <div>
-      <Link to="/carrinho" data-testid="shopping-cart-button">Carrinho de compras</Link>
-      <input type="text" />
-      <span data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </span>
-      <Categories />
-    </div>
-  );
->>>>>>> a1123455137d774ba6a2e0b97e858a45f0336acf
 }
 
 export default Search;
